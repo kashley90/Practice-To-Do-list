a674db16b8f40e4defc8292091ea06a0611d5762
Kyle Ashley <89463912+kashley90@users.noreply.github.com>
2022-09-05 16:19:44 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import React, {useState} from "react";
+import './App.css';
+
+function App() {
+  const [newTodo, setNewTodo]= useState("");
+  const[todos, setTodos]= useState([]);
+  
+  
+
+  const handleNewTodoSubmit = (event)=>{
+    event.preventDefault();
+
+  const todoItem = {
+    text: newTodo,
+    complete: false
+  }
+
+    setTodos([...todos,todoItem]);
+    setNewTodo("");
+  };
+  const handleTodoDelete = (delIdx) => {
+    const filteredTodos = todos.filter((todo, i)=> {
+      return i !== delIdx;
+    });
+    setTodos(filteredTodos);
+  };
+  const handleToggleComplete=(idx)=> {
+    const updatedTodos = todos.map((todo, i)=> {
+      if (idx = i){
+        todo.complete = !todo.complete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  }
+
+  return (
+    <div style={{textAlign:"center"}} >
+      <form onSubmit={(event)=>{
+        handleNewTodoSubmit(event);
+      }}>
+        <input onChange={(event)=>{
+          setNewTodo(event.target.value);
+        }} 
+        type="text" 
+        value={newTodo}   
+        
+        
+        />
+      <div>
+        <button>Add</button>
+      </div>
+        </form>
+      {
+        todos.map((todo, i)=>{
+          const todoClasses = [];
+
+          if (todo.complete) {
+            todoClasses.push("line-throught");
+          }
+          
+          return (
+          <div key={i}>
+            <input onChange={(event)=>{
+              handleToggleComplete(i);
+            }} checked={todo.compplete}type="checkbox"></input>
+            <span className={todoClasses.join("")}>{todo.text}</span>
+            <button onClick={(event)=>{
+              handleTodoDelete(i);
+            }}>Delete</button>
+          </div>
+        );
+      })}  
+    </div>
+  );
+}
+
+export default App;
